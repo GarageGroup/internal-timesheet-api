6fb5046658319e48eefbf2639999c0b5cc8cface
Dmitry Panchuk <dpanchuk@ggroupp.ru>
2021-11-17 15:48:49 +0300

    SetSearchFunc => SetSearch (change naming)

---
+++ src/ProjectSet.Search/ProjectSet.Search.Api/ProjectSetSearchDependency.cs
@@ -0,0 +1,16 @@
+﻿using GGroupp.Infra;
+using PrimeFuncPack;
+using System;
+
+namespace GGroupp.Internal.Timesheet;
+
+using IProjectSetSearchFunc = IAsyncValueFunc<ProjectSetSearchIn, Result<ProjectSetSearchOut, Failure<ProjectSetSearchFailureCode>>>;
+
+public static class ProjectSetSearchDependency
+{
+    public static Dependency<IProjectSetSearchFunc> UseProjectSetSearchApi<IDataVerseApiClient>(
+        this Dependency<IDataVerseApiClient> dependency)
+        where IDataVerseApiClient : IDataverseSearchSupplier
+        =>
+        dependency.Map<IProjectSetSearchFunc>(apiClient => ProjectSetSearchFunc.Create(apiClient));
+}
